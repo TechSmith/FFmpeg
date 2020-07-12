af6764dcd9af84b893e8b305f06730a540df3030
Hool, Rory <hoolrory@gmail.com>
2020-07-11 22:28:42 -0400

    Pushing this up to see if it works on macos 10.14
    Getting "Bad CPU type in executable" on 10.15, which may be a 32-bit thing

--- Mac/build.sh
+++ Mac/build.sh
@@ -4,7 +4,16 @@ rm -rf include
 
 cd ..
 
+ARCH="arm64"
+#SYSROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
+#export CFLAGS="-arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SYSROOT} -fembed-bitcode -Werror=partial-availability"
+#export LDFLAGS="-arch ${ARCH} -isysroot ${SYSROOT}"
+#export CXXFLAGS="-arch ${ARCH} -isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
+
+# --disable-debug --target=arm64-apple-macos11 --disable-fvisibility --host=arm-apple-darwin
+
 ./configure \
+   --arch="arm64"
    --enable-libmp3lame \
    --disable-encoders \
    --enable-encoder="h264_videotoolbox,h265_videotoolbox,aac_at,pcm*,libmp3lame" \
